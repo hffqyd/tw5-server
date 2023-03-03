f7465a280c83d3ea01d4e38df183b2c8bcd10234
hffqyd <hffqyd@sina.com>
2023-03-03 09:59:57 +0800

    rewrite server in Nim, with additional features

---
+++ tw5server.nim
@@ -0,0 +1,332 @@
+import
+  asynchttpserver,
+  asyncdispatch,
+  os,
+  strutils,
+  times,
+  parseopt,
+  sequtils,
+  algorithm,
+  sets,
+  uri,
+  zippy,
+  mimetypes
+
+import tables, strtabs
+from jester/private/utils import parseMPFD
+
+type MultiData* = OrderedTable[string, tuple[fields: StringTableRef, body: string]]
+
+from httpcore import HttpMethod, HttpHeaders
+
+const
+  name = "TW5 server"
+  version = "1.0.0"
+  style = staticRead("style.css")
+  temp = staticRead("template.html")
+
+const usage = """
+tw5server -a:localhost -p:8000 -d:dir -b:backup
+
+-h usage help
+-a address, defautl "127.0.0.1"
+-p port, default 8000
+-d directory to servering, default `current dir`
+-b backup directory name, default `backup`
+-l show log message
+
+Backups auto-clean strategy:
+Keep all backups in current month, keep only the newest one for previous months.
+"""
+
+proc time_now(): string =
+  return now().format("yyyyMMddHHmmss")
+
+type
+  NimHttpResponse = tuple[
+    code: HttpCode,
+    content: string,
+    headers: HttpHeaders
+    ]
+
+  NimHttpSettings = object
+    directory: string
+    mimes: MimeDb
+    port: Port
+    title: string
+    address: string
+    name: string
+    version: string
+
+# TODO: update web page after upload
+proc h_page(settings:NimHttpSettings, content, title, subtitle: string): string =
+  var footer = """<div id="footer">$1 v$2</div>""" % [settings.name, settings.version]
+  result = temp % [title, style, subtitle, content, footer]
+
+proc relativePath(path, cwd: string): string =
+  var path2 = path
+  if cwd == "/":
+    return path
+  else:
+    path2.delete(0..cwd.len-1)
+  var relpath = path2.replace("\\", "/")
+  if (not relpath.endsWith("/")) and (not path.fileExists):
+    relpath = relpath&"/"
+  if not relpath.startsWith("/"):
+    relpath = "/"&relpath
+  return relpath
+
+proc relativeParent(path, cwd: string): string =
+  var relparent = path.parentDir.relativePath(cwd)
+  if relparent == "":
+    return "/"
+  else:
+    return relparent
+
+
+proc sendNotFound(settings: NimHttpSettings, path: string): NimHttpResponse =
+  var content = "<p>The page you requested cannot be found.<p>"
+  return (code: Http404, content: h_page(settings, content, $int(Http404), "Not Found"), headers: {"Content-Type": "text/html"}.newHttpHeaders())
+
+
+proc sendStaticFile(settings: NimHttpSettings, path: string): NimHttpResponse =
+  let mimes = settings.mimes
+  var ext = path.splitFile.ext
+  if ext == "":
+    ext = ".txt"
+  ext = ext[1 .. ^1]
+  let mimetype = mimes.getMimetype(ext.toLowerAscii)
+  var file = path.readFile
+  return (code: Http200, content: file, headers: {"Content-Type": mimetype}.newHttpHeaders)
+
+
+proc sendDirContents(settings: NimHttpSettings, dir: string): NimHttpResponse =
+  let cwd = settings.directory.absolutePath
+  var res: NimHttpResponse
+  var files = newSeq[string](0)
+  var path = dir.absolutePath
+  if not path.startsWith(cwd):
+    path = cwd
+  if path != cwd and path != cwd&"/" and path != cwd&"\\":
+    files.add """<li class="i-back entypo"><a href="$1">..</a></li>""" % [path.relativeParent(cwd)]
+  var title = settings.title
+  let subtitle = path.relativePath(cwd)
+  for i in walkDir(path):
+    let name = i.path.extractFilename
+    let relpath = i.path.relativePath(cwd)
+    if name == "index.html" or name == "index.htm":
+      return sendStaticFile(settings, i.path)
+    if i.path.dirExists:
+      files.add """<li class="i-folder entypo"><a href="$1">$2</a></li>""" % [relpath, name]
+    else:
+      files.add """<li class="i-file entypo"><a href="$1">$2</a></li>""" % [relpath, name]
+  let ul = """
+<ul>
+  $1
+</ul>
+""" % [files.join("\n")]
+  res = (code: Http200, content: h_page(settings, ul, title, subtitle), headers: {"Content-Type": "text/html"}.newHttpHeaders())
+  return res
+
+proc sendOptions(): NimHttpResponse =
+  var header = {"status": "ok", "dav": "tw5/put", "allow": "GET,HEAD,POST,OPTIONS,CONNECT,PUT,DAV,dav", "x-api-access-type": "file"}
+  return (code: Http200, content: "", headers: header.newHttpHeaders())
+
+proc logmsg(msg: string, log: bool) =
+  if log:
+    echo msg
+
+proc getPut(req: Request, path, backup: string, log: bool): NimHttpResponse = 
+  let content = req.body
+  writeFile(path, content)
+  logmsg("Update: " & path, log)
+
+  let (dir, name, _) = splitFile(path)
+  let backup_name = dir / backup / name & "-" & time_now() & ".html.gz"
+
+  let compressed = compress(content, BestCompression)
+  writeFile(backup_name, compressed)
+  logmsg("Backup to: " & backup_name, log)
+
+  return (code: Http200, content: "saved", headers: {"Content-Type": "text;charset=UTF-8"}.newHttpHeaders())
+
+proc savePost(req: Request, path, url_path: string, log: bool): NimHttpResponse =
+  let
+    header = req.headers
+    contentType = header.getOrDefault("Content-Type")
+    body = parseMPFD(contentType, req.body)
+    file = body["file"]
+    filename = file.fields["filename"]
+    file_body = file.body
+    override = body.getOrDefault("write").body
+
+  var
+    rsp_content = ""
+    code = Http400
+
+  if fileExists(path / filename) and "yes" != override:
+    let
+      (_, base, ext) = filename.splitFile()
+      newName = base & "-" & time_now() & ext
+
+    writeFile(path / newName, file_body)
+    let
+      rsp_msg = "Save file to " & newName 
+      msg = rsp_msg & " in " & path
+    rsp_content = rsp_msg
+    code = Http200
+    logmsg(msg, log)
+  else:
+    writeFile(path / filename, file_body)
+    let
+      rsp_msg = "Save file to " & filename
+      msg = rsp_msg & " in " & path
+    rsp_content = rsp_msg
+    code = Http200
+    logmsg(msg, log)
+  return (code: code, content: rsp_content, headers: {"status": "ok"}.newHttpHeaders())
+
+proc serve(settings: NimHttpSettings, backup: string, log: bool) =
+  var server = newAsyncHttpServer()
+  proc handleHttpRequest(req: Request): Future[void] {.async.} =
+    let
+      url_path = req.url.path.replace("%20", " ").decodeUrl()
+      path = settings.directory / url_path
+    var res: NimHttpResponse
+    case req.reqMethod:
+      of HttpGet:
+        if path.dirExists:
+          res = sendDirContents(settings, path)
+        elif path.fileExists:
+          res = sendStaticFile(settings, path)
+        else:
+          res = sendNotFound(settings, path)
+      of HttpPut:
+        res = getPut(req, path, backup, log)
+      of HttpOptions:
+        res = sendOptions()
+      of HttpHead:
+        res = sendOptions()
+      of HttpPost:
+        res = savePost(req, path, url_path, log)
+      else:
+        echo(req.reqMethod)
+    await req.respond(res.code, res.content, res.headers)
+  asyncCheck server.serve(settings.port, handleHttpRequest, settings.address)
+
+proc currentMonth(ymd: string): string =
+  # 20230123 -> 202301
+  return ymd[0..<6]
+
+proc old_backups(path, name: string): seq[string] =
+  let
+    now = time_now()
+    c_year_month = currentMonth(now)
+
+    backup_name = path / name
+    all_backup_unsort = toSeq(walkPattern(backup_name & "*.html.gz"))
+    all_backup = sorted(all_backup_unsort, Descending)
+
+  var
+    to_be_removed: seq[string]
+    saved_y_m = ""
+
+  for i in all_backup:
+    let
+      date = i[^22..^1]
+      y_m = currentMonth(date)
+
+    if y_m >= c_year_month:
+      continue
+
+    if saved_y_m == y_m:
+      to_be_removed.add(i)
+    else:
+      saved_y_m = y_m
+
+  return to_be_removed
+
+proc backupFileName(name: string): string =
+  # backup name: name-timestamp.html.gz, e.g, test-20230227142037.html.gz
+  return name[0..^21]
+
+proc clean_backup(dir, backup: string): int =
+  var names: HashSet[string]
+  let all_backups = toSeq(walkPattern(dir / backup / "*.html.gz"))
+
+  for i in all_backups:
+    let (_, name, _) = splitFile(i)
+    names.incl(backupFileName(name))
+
+  var count = 0
+  for i in names:
+    for old in old_backups(dir / backup, i):
+      removeFile(old)
+      count += 1
+
+  return count
+
+
+var
+  port = 8000
+  address = "127.0.0.1"
+  dir = getCurrentDir()
+  backup = "backup"
+  title = "TW5 server"
+  log = false
+
+for kind, key, val in parseopt.getopt():
+  case kind
+  of cmdArgument:
+    continue
+  of cmdShortOption, cmdLongOption:
+    case key
+    of "h", "help":
+      echo usage
+      quit()
+    of "a", "address":
+      address = val
+    of "p", "port":
+      port = parseInt(val)
+    of "d", "dir":
+      dir = val
+    of "b", "backup":
+      backup = val
+    of "l", "log":
+      log = true
+  else:
+    assert(false)
+
+var settings: NimHttpSettings
+settings.directory = dir
+settings.mimes = newMimetypes()
+settings.mimes.register("htm", "text/html")
+settings.address = address
+settings.name = name
+settings.title = title
+settings.version = version
+settings.port = Port(port)
+
+echo(" Serving url: ", address, ":", port)
+echo("Serving path: ", dir)
+echo("  Backup dir: ", backup)
+
+proc handleCtrlC() {.noconv.} =
+  write(stdout, "\rClean backups (y to clean): ")
+  let clean = readLine(stdin)
+  
+  var cleaned = 0
+  if "y" == clean:
+    cleaned = clean_backup(dir, backup)
+
+  if cleaned > 0:
+    echo(cleaned, " backup(s) cleaned. Bye ~")
+  else:
+    echo("No backups were cleaned. Bye ~")
+
+  quit()
+
+setControlCHook(handleCtrlC)
+
+serve(settings, backup, log)
+runForever()
